5cbd4b3e5c16b08c19594b2a0c33e1cd1627a284
Martin Buchner <martin-buchner@gmx.de>
2022-01-25 23:04:39 +0800

    Remove duplicate comment from nvim-treesitter config

--- plug-config/nvim-treesitter.lua
+++ plug-config/nvim-treesitter.lua
@@ -2,8 +2,6 @@ require('nvim-treesitter.configs').setup({
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = "maintained",
 
-  -- ignore_install = { "haskell", "jsonc" },
-
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,
 

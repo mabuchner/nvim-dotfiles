034b61b67eb9fb4457de9f04f8e8622cb8d0f039
Martin Buchner <martin-buchner@gmx.de>
2022-03-10 22:58:28 +0800

    Reformat lua config files

--- plug-config/lspconfig.lua
+++ plug-config/lspconfig.lua
@@ -1,14 +1,14 @@
-local nvim_lsp = require('lspconfig')
+local nvim_lsp = require("lspconfig")
 
 -- Show line diagnostics automatically in hover window
 vim.o.updatetime = 250
 -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 -- For diagnostics at specific cursor position
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
+vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]])
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
+capabilities.textDocument.completion.completionItem.documentationFormat = { "markdown", "plaintext" }
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.preselectSupport = true
 capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
@@ -17,10 +17,9 @@ capabilities.textDocument.completion.completionItem.deprecatedSupport = true
 capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
 capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
 capabilities.textDocument.completion.completionItem.resolveSupport = {
-  properties = {
-    'documentation',
-    'detail',
-    'additionalTextEdits',
-  },
+	properties = {
+		"documentation",
+		"detail",
+		"additionalTextEdits",
+	},
 }
-
--- plug-config/vim-better-whitespace.lua
+++ plug-config/vim-better-whitespace.lua
@@ -1,6 +1,5 @@
 local g = vim.g
 
 -- Remove gitcommit and markdown from the blacklist
-g.better_whitespace_filetypes_blacklist = { 'diff', 'unite', 'help' }
+g.better_whitespace_filetypes_blacklist = { "diff", "unite", "help" }
 g.show_spaces_that_precede_tabs = 1
-

489da1e73c68e67436e9c1345509400b58a3fb0d
Martin Buchner <martin.buchner@fraunhofer.sg>
2022-03-22 17:26:00 +0800

    Use lspkind plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,6 +51,7 @@ Plug("hrsh7th/cmp-vsnip")
 Plug("hrsh7th/vim-vsnip")
 Plug("jose-elias-alvarez/null-ls.nvim")
 Plug("nvim-lua/plenary.nvim") -- Required by null-ls
+Plug("onsails/lspkind-nvim") -- Add pictograms to lsp completion
 
 -- lualine
 Plug("nvim-lualine/lualine.nvim")

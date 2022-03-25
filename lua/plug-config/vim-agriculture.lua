a801366ec6a1cf31905ac7f70405cbe9186133d2
Martin Buchner <martin.buchner@fraunhofer.sg>
2022-03-25 10:50:53 +0800

    Remove vim-agriculture config

--- lua/plug-config/vim-agriculture.lua
+++
@@ -1,5 +0,0 @@
-vim.cmd("nmap <leader>/ <Plug>RgRawSearch")
-vim.cmd("vmap <leader>/ <Plug>RgRawVisualSelection")
-vim.cmd("nmap <leader>* <Plug>RgRawWordUnderCursor")
-
-vim.g["agriculture#rg_options"] = "--smart-case"

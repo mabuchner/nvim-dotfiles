e363d3c3150804463894ea9c45ce710d3133f4f3
Martin Buchner <martin.buchner@fraunhofer.sg>
2022-03-25 10:26:22 +0800

    Use live_grep_raw as alternative to agriculture

--- lua/plug-config/telescope.lua
+++ lua/plug-config/telescope.lua
@@ -13,9 +13,11 @@ telescope.setup({
 })
 
 telescope.load_extension("fzf")
+telescope.load_extension("live_grep_raw")
 
 vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<cr>", { noremap = true })
+--vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep_raw<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<cr>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>f*", ":Telescope grep_string<cr>", { noremap = true })

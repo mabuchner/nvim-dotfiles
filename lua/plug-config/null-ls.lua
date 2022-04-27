8c96cc5143182121ec0e5a8201952597a68a197f
Martin Buchner <mbuchner@indeed.com>
2022-04-27 11:21:26 +0800

    Disable null-ls clang-format formatting due to encoding error

--- lua/plug-config/null-ls.lua
+++ lua/plug-config/null-ls.lua
@@ -3,7 +3,6 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.cmake_format,
 		-- null_ls.builtins.formatting.lua_format,
 		null_ls.builtins.formatting.latexindent,

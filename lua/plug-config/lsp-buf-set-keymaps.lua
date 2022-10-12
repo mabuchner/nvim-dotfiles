4fbb5054b41d22d7ce2286d9132371010abc46aa
Martin Buchner <mbuchner@indeed.com>
2022-10-12 10:41:23 +0800

    Move LSP keybindings into separate function

---
+++ lua/plug-config/lsp-buf-set-keymaps.lua
@@ -0,0 +1,65 @@
+local opts = { noremap = true, silent = true }
+
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.api.nvim_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+vim.api.nvim_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
+vim.api.nvim_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+
+local lsp_buf_set_keymaps = function(bufnr)
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	-- LSP
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		"n",
+		"<space>wl",
+		"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
+		opts
+	)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "v", "<space>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+
+	-- Debugger
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<F5>", "<cmd>lua require('dap').continue()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<F10>", "<cmd>lua require('dap').step_over()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<F11>", "<cmd>lua require('dap').step_into()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<F12>", "<cmd>lua require('dap').step_out()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>b", "<cmd>lua require('dap').toggle_breakpoint()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		"n",
+		"<space>B",
+		"<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+		opts
+	)
+	vim.api.nvim_buf_set_keymap(
+		bufnr,
+		"n",
+		"<space>lp",
+		"<cmd>lua require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+		opts
+	)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>dr", "<cmd>lua require('dap').repl.open()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>dl", "<cmd>lua require('dap').run_last()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<M-k>", "<cmd>lua require('dapui').eval()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "v", "<M-k>", "<cmd>lua require('dapui').eval()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>df", "<cmd>lua require('jdtls').test_class()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<space>dn", "<cmd>lua require('jdtls').test_nearest_method()<CR>", opts)
+end
+
+return lsp_buf_set_keymaps

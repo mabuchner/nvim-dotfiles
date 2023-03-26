1ec3f5d64c710bfed3da4f56c41ad41c954c7040
Martin Buchner <martin.buchner@fraunhofer.sg>
2023-03-26 23:32:22 +0800

    Hide trailing whitespace in terminal

--- lua/plug-config/vim-better-whitespace.lua
+++ lua/plug-config/vim-better-whitespace.lua
@@ -3,3 +3,11 @@ local g = vim.g
 -- Remove gitcommit and markdown from the blacklist
 g.better_whitespace_filetypes_blacklist = { "diff", "unite", "help" }
 g.show_spaces_that_precede_tabs = 1
+
+-- Workaround to hide trailing whitespace in the terminal
+-- (https://github.com/ntpeters/vim-better-whitespace/issues/158)
+local betterWhitespaceGrp = vim.api.nvim_create_augroup("BetterWhiteSpaceGrp", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+    command = "DisableWhitespace",
+    group = betterWhitespaceGrp,
+})

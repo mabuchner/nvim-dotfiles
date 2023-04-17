0d0594eabfcedd78f176ba139e5a07ab28a9f1a8
Martin Buchner <martin.buchner@fraunhofer.sg>
2023-04-17 23:03:11 +0800

    Add debugger config for C/C++/Rust

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,5 +1,54 @@
+local configLLDB = function(dap)
+    dap.adapters.lldb = {
+        type = "executable",
+        command = "/opt/homebrew/opt/llvm/bin/lldb-vscode",
+        name = "lldb",
+    }
+
+    dap.configurations.cpp = {
+        {
+            name = "Launch",
+            type = "lldb",
+            request = "launch",
+            program = function()
+                return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopOnEntry = false,
+            args = {},
+
+            -- 💀
+            -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+            --
+            --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+            --
+            -- Otherwise you might get the following error:
+            --
+            --    Error on launch: Failed to attach to the target process
+            --
+            -- But you should be aware of the implications:
+            -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+            -- runInTerminal = false,
+        },
+        {
+            -- If you get an "Operation not permitted" error using this, try disabling YAMA:
+            --  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+            name = "Attach to process",
+            type = "cpp", -- Adjust this to match your adapter name (`dap.adapters.<name>`)
+            request = "attach",
+            pid = require("dap.utils").pick_process,
+            args = {},
+        },
+    }
+
+    dap.configurations.c = dap.configurations.cpp
+    dap.configurations.rust = dap.configurations.cpp
+end
+
 local config = function()
     local dap = require("dap")
+    configLLDB(dap)
+
     local dapui = require("dapui")
 
     require("nvim-dap-virtual-text").setup()

1b48d0244414f04b18b8faaaab591e335a99f784
martin.buchner <martin.buchner@bytedance.com>
2023-09-04 13:41:55 +0800

    Improve CMP keybindings
    Previously pressing tab would jump to some random location.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,6 +2,7 @@ local config = function()
     vim.o.completeopt = "menu,menuone,noselect"
 
     local has_words_before = function()
+        unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
@@ -33,6 +34,7 @@ local config = function()
     end
 
     cmp.setup({
+        preselect = cmp.PreselectMode.None,
         snippet = {
             -- REQUIRED - you must specify a snippet engine
             expand = function(args)
@@ -50,13 +52,9 @@ local config = function()
             ["<C-b>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-Space>"] = cmp.mapping.complete(),
-            -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
             ["<C-e>"] = cmp.mapping.abort(),
             ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
             ["<Tab>"] = cmp.mapping(function(fallback)
-                -- if cmp.visible() then
-                -- cmp.select_next_item()
-                -- elseif luasnip.expand_or_jumpable() then
                 if luasnip.expand_or_jumpable() then
                     luasnip.expand_or_jump()
                 elseif has_words_before() then
@@ -67,9 +65,7 @@ local config = function()
             end, { "i", "s" }),
 
             ["<S-Tab>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
+                if luasnip.jumpable(-1) then
                     luasnip.jump(-1)
                 else
                     fallback()

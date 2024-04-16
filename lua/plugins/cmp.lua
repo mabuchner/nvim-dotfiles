8c362e85948046b403cb97b42fe93f826fb69759
Martin Buchner <martin.buchner@fraunhofer.sg>
2024-04-16 22:50:34 +0800

    Exit lua snip when leaving insert mode
    Source: https://github.com/L3MON4D3/LuaSnip/issues/656

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -33,6 +33,23 @@ local config = function()
         end
     end
 
+    vim.api.nvim_create_autocmd("ModeChanged", {
+        group = vim.api.nvim_create_augroup("UnlinkLuaSnipSnippetOnModeChange", {
+            clear = true,
+        }),
+        pattern = { "s:n", "i:*" },
+        desc = "Forget the current snippet when leaving the insert mode",
+        callback = function(evt)
+            while true do
+                if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+                    luasnip.unlink_current()
+                else
+                    break
+                end
+            end
+        end,
+    })
+
     cmp.setup({
         preselect = cmp.PreselectMode.None,
         snippet = {

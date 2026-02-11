ebe4e657ff2cf2941e9ecc4c049add211364180a
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:54:11 +0800

    Build LuaSnip's jsregexp after install

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -173,6 +173,7 @@ return {
     },
     {
         "L3MON4D3/LuaSnip",
+        build = "make install_jsregexp",
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
         end,

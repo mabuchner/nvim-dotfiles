41655ab7c23b28b9d8192943b12fa8aaf10f9980
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:54:12 +0800

    Use rounder border for LSP hover window

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,6 +33,15 @@ local config = function()
         debounce_text_changes = 150,
     }
 
+    -- Borders
+
+    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+        opts = opts or {}
+        opts.border = opts.border or "rounded"
+        return orig_util_open_floating_preview(contents, syntax, opts, ...)
+    end
+
     mason_lspconfig.setup_handlers({
         function(server_name)
             lspconfig[server_name].setup({

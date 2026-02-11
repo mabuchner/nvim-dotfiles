3103d40685a6d2694c9dbd14d193de7442e138c3
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:54:09 +0800

    Use lspkind plugin

--- lua/plug-config/cmp.lua
+++ lua/plug-config/cmp.lua
@@ -10,6 +10,7 @@ local feedkey = function(key, mode)
 end
 
 local cmp = require("cmp")
+local lspkind = require("lspkind")
 
 cmp.setup({
 	snippet = {
@@ -64,6 +65,18 @@ cmp.setup({
 	}),
 	experimental = {
 		ghost_text = true,
+	},
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol_text", -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
 	},
 })
 

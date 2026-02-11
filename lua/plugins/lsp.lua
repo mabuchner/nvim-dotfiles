1584f64d191294f318ff321858f75b300646e3d1
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:54:12 +0800

    Remove deprecated setup_handlers
    https://github.com/mason-org/mason-lspconfig.nvim/issues/545#issuecomment-2925270862
    
    https://github.com/mason-org/mason-lspconfig.nvim/releases/tag/v2.0.0

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,144 +1,130 @@
 local config = function()
-    -- Show line diagnostics automatically in hover window
-    vim.o.updatetime = 250
-    -- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-    -- For diagnostics at specific cursor position
-    vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]])
+	-- Show line diagnostics automatically in hover window
+	vim.o.updatetime = 250
+	-- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+	-- For diagnostics at specific cursor position
+	vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]])
 
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
-    local lspconfig = require("lspconfig")
+	local mason = require("mason")
+	local mason_lspconfig = require("mason-lspconfig")
+	local lspconfig = require("lspconfig")
 
-    local navic = require("nvim-navic")
+	local navic = require("nvim-navic")
 
-    local lsp_buf_set_keymaps = require("plug-config/lsp-buf-set-keymaps")
+	local lsp_buf_set_keymaps = require("plug-config/lsp-buf-set-keymaps")
 
-    local on_attach = function(client, bufnr)
-        if client.server_capabilities.documentSymbolProvider then
-            navic.attach(client, bufnr)
-        end
+	local on_attach = function(client, bufnr)
+		if client.server_capabilities.documentSymbolProvider then
+			navic.attach(client, bufnr)
+		end
 
-        lsp_buf_set_keymaps(bufnr)
-    end
+		lsp_buf_set_keymaps(bufnr)
+	end
 
-    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-    local capabilities = cmp_nvim_lsp.default_capabilities()
+	local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    -- Specify offset encoding for clangd to avoid conflicts with null-ls
-    local capabilities_clangd = cmp_nvim_lsp.default_capabilities()
-    capabilities_clangd.offsetEncoding = "utf-8"
+	-- Specify offset encoding for clangd to avoid conflicts with null-ls
+	local capabilities_clangd = cmp_nvim_lsp.default_capabilities()
+	capabilities_clangd.offsetEncoding = "utf-8"
 
-    local lsp_flags = {
-        debounce_text_changes = 150,
-    }
+	local lsp_flags = {
+		debounce_text_changes = 150,
+	}
 
-    -- Borders
+	-- Borders
 
-    local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-    function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-        opts = opts or {}
-        opts.border = opts.border or "rounded"
-        return orig_util_open_floating_preview(contents, syntax, opts, ...)
-    end
+	local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+	function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+		opts = opts or {}
+		opts.border = opts.border or "rounded"
+		return orig_util_open_floating_preview(contents, syntax, opts, ...)
+	end
 
-    mason_lspconfig.setup_handlers({
-        function(server_name)
-            lspconfig[server_name].setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-                flags = lsp_flags,
-            })
-        end,
-        ["clangd"] = function()
-            lspconfig.clangd.setup({
-                on_attach = on_attach,
-                capabilities = capabilities_clangd,
-                flags = lsp_flags,
-            })
-        end,
-        ["gopls"] = function()
-            lspconfig.gopls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-                flags = lsp_flags,
-                settings = {
-                    gopls = {
-                        completeUnimported = true,
-                        usePlaceholders = true,
-                        analyses = {
-                            nilness = true,
-                            shadow = true,
-                            unusedparams = true,
-                            unusedvariable = true,
-                            unusedwrite = true,
-                        },
-                    },
-                },
-            })
-        end,
-        ["rust_analyzer"] = function()
-            lspconfig.rust_analyzer.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-                flags = lsp_flags,
-                settings = {
-                    ["rust-analyzer"] = {
-                        checkOnSave = {
-                            command = "clippy",
-                        },
-                    },
-                },
-            })
-        end,
-        ["lua_ls"] = function()
-            lspconfig.lua_ls.setup({
-                on_attach = on_attach,
-                capabilities = capabilities,
-                flags = lsp_flags,
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" },
-                        },
-                    },
-                },
-            })
-        end,
-    })
+	vim.lsp.config("clangd", {
+		on_attach = on_attach,
+		capabilities = capabilities_clangd,
+		flags = lsp_flags,
+	})
 
-    mason.setup()
-    mason_lspconfig.setup()
+	vim.lsp.config("gopls", {
+		on_attach = on_attach,
+		capabilities = capabilities,
+		flags = lsp_flags,
+		settings = {
+			gopls = {
+				completeUnimported = true,
+				usePlaceholders = true,
+				analyses = {
+					nilness = true,
+					shadow = true,
+					unusedparams = true,
+					unusedvariable = true,
+					unusedwrite = true,
+				},
+			},
+		},
+	})
+
+	vim.lsp.config("rust_analyzer", {
+		on_attach = on_attach,
+		capabilities = capabilities,
+		flags = lsp_flags,
+		settings = {
+			["rust-analyzer"] = {
+				checkOnSave = {
+					command = "clippy",
+				},
+			},
+		},
+	})
+
+	vim.lsp.config("lua_ls", {
+		on_attach = on_attach,
+		capabilities = capabilities,
+		flags = lsp_flags,
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
+				},
+			},
+		},
+	})
+
+	mason.setup()
+	mason_lspconfig.setup()
 end
 
 return {
-    {
-        "neovim/nvim-lspconfig",
-        config = config,
-        dependencies = {
-            -- Automatically install LSPs to stdpath for neovim
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
+	{
+		"neovim/nvim-lspconfig",
+		config = config,
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
 
-            -- Loading indicator
-            {
-                "j-hui/fidget.nvim",
-                tag = "legacy", -- As recommended by the author while code is being rewritten
-                config = true,
-            },
+			-- Loading indicator
+			{
+				"j-hui/fidget.nvim",
+				tag = "legacy", -- As recommended by the author while code is being rewritten
+				config = true,
+			},
 
-            -- Component to get code
-            "SmiteshP/nvim-navic",
-            "hrsh7th/cmp-nvim-lsp",
-        },
-    },
-    {
-        "ray-x/lsp_signature.nvim",
-        opts = {
-            bind = true, -- This is mandatory, otherwise border config won't get registered.
-            handler_opts = {
-                border = "rounded",
-            },
-        },
-    },
+			-- Component to get code
+			"SmiteshP/nvim-navic",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	},
+	{
+		"ray-x/lsp_signature.nvim",
+		opts = {
+			bind = true, -- This is mandatory, otherwise border config won't get registered.
+			handler_opts = {
+				border = "rounded",
+			},
+		},
+	},
 }

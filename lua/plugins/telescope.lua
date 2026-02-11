9a26a8f9c687dccb4589066cc0c63d21ead88392
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:54:12 +0800

    List LSP functions in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -15,6 +15,10 @@ vim.keymap.set("n", "<leader>fk", function()
     require("telescope.builtin").keymaps()
 end)
 
+vim.keymap.set("n", "<leader>fn", function()
+    require('telescope.builtin').lsp_document_symbols({ symbols = 'function' })
+end)
+
 local config = function()
     local telescope = require("telescope")
 
@@ -26,10 +30,10 @@ local config = function()
         },
         extensions = {
             fzf = {
-                fuzzy = true,       -- false will only do exact matching
+                fuzzy = true,                   -- false will only do exact matching
                 override_generic_sorter = true, -- override the generic sorter
-                override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                override_file_sorter = true,    -- override the file sorter
+                case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
                 -- the default case_mode is "smart_case"
             },
         },

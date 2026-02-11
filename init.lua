394807898484775002da3a6da8328a61ca6d8bd2
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:55:02 +0800

    Move Lazy init to separate file

--- init.lua
+++ init.lua
@@ -82,18 +82,4 @@ g.netrw_bufsettings = "noma nomod nu nobl nowrap ro"
 
 require("keys")
 
--- Install plugin manager and load plugins
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
+require("config.lazy")

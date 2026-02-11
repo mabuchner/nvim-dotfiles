177782f8136127bc16f830d7fe20c9fcf6f0be1c
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:55:02 +0800

    Attach nvim-navic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,21 +13,33 @@ return {
 			},
 		},
 	},
-	{
-		"SmiteshP/nvim-navic",
-		opts = {
-			auto_attach = true,
-		},
-	},
+	-- {
+	-- 	"SmiteshP/nvim-navic",
+	-- 	opts = {
+	-- 		auto_attach = true,
+	-- 	},
+	-- },
 	{
 		"hrsh7th/cmp-nvim-lsp",
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
 			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 			local capabilities = cmp_nvim_lsp.default_capabilities()
+
+			local navic = require("nvim-navic")
+			local on_attach = function(client, bufnr)
+				if client.server_capabilities.documentSymbolProvider then
+					navic.attach(client, bufnr)
+				end
+			end
+
 			vim.lsp.config("*", {
 				capabilities = capabilities,
+				on_attach = on_attach,
 			})
 		end,
+		dependencies = {
+			"SmiteshP/nvim-navic",
+		},
 	},
 }

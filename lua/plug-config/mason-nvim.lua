38028520cf2957c5eb8d4d7f1bf51bb462653d53
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:54:10 +0800

    Run clippy when saving rust files

--- lua/plug-config/mason-nvim.lua
+++ lua/plug-config/mason-nvim.lua
@@ -5,37 +5,51 @@ local lspconfig = require("lspconfig")
 local lsp_buf_set_keymaps = require("plug-config/lsp-buf-set-keymaps")
 
 local on_attach = function(client, bufnr)
-	lsp_buf_set_keymaps(bufnr)
+    lsp_buf_set_keymaps(bufnr)
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local lsp_flags = {
-	debounce_text_changes = 150,
+    debounce_text_changes = 150,
 }
 
 mason_lspconfig.setup_handlers({
-	function(server_name)
-		lspconfig[server_name].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			flags = lsp_flags,
-		})
-	end,
-	["sumneko_lua"] = function()
-		lspconfig.sumneko_lua.setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-			flags = lsp_flags,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		})
-	end,
+    function(server_name)
+        lspconfig[server_name].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            flags = lsp_flags,
+        })
+    end,
+    ["rust_analyzer"] = function()
+        lspconfig.rust_analyzer.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            flags = lsp_flags,
+            settings = {
+                ["rust-analyzer"] = {
+                    checkOnSave = {
+                        command = "clippy",
+                    },
+                },
+            },
+        })
+    end,
+    ["sumneko_lua"] = function()
+        lspconfig.sumneko_lua.setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            flags = lsp_flags,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                },
+            },
+        })
+    end,
 })
 
 mason.setup()

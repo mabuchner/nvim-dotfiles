0bdb0d4493a01780027b957f26d38bdcc2d65a0d
Martin Buchner <6006874+mabuchner@users.noreply.github.com>
2026-02-11 23:54:09 +0800

    Remove width from lspkind config

--- lua/plug-config/cmp.lua
+++ lua/plug-config/cmp.lua
@@ -68,8 +68,7 @@ cmp.setup({
 	},
 	formatting = {
 		format = lspkind.cmp_format({
-			mode = "symbol_text", -- show only symbol annotations
-			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			mode = "symbol_text",
 
 			-- The function below will be called before any actual modifications from lspkind
 			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
